961f517dfdbad6524199fdc200d50870b927a5e5
Willy Kantana <willy@alegrium.com>
2021-11-17 18:32:22 +0700

    rotating collider

---
+++ bounceleague/Assets/Resources/Script/RotatingObstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObstacle : MonoBehaviour
+{
+    public float rotationSpeed = 1;
+    public bool rotatingClockWise;
+
+    Vector3 rot;
+    int dir;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir = rotatingClockWise ? 1 : -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rot = Vector3.forward * (rotationSpeed * dir * Time.deltaTime * 100f);
+        Debug.Log(string.Format("Rotate to: [{0},{1},{2}]", rot.x, rot.y, rot.z));
+        transform.Rotate(rot);
+    }
+}
