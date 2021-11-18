b43ae92fc1c5441e20e3b61c03e5940544c95366
Yoga <yoga@alegrium.com>
2021-11-18 12:01:58 +0700

    - Basic movement controller

---
+++ bounceleague/Assets/Resources/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Character : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float speed;
+    Vector2 move;
+    InputController inputController;
+
+    void Awake()
+    {
+        inputController = new InputController();
+        
+        inputController.PlayerA.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        inputController.PlayerA.Move.canceled += ctx => move = Vector2.zero;
+        
+        inputController.PlayerA.Look.performed += ctx => move = ctx.ReadValue<Vector2>();
+        inputController.PlayerA.Look.canceled += ctx => move = Vector2.zero;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 m = new Vector3(move.x,move.y,0);
+    }
+
+    void OnEnable()
+    {
+        inputController.PlayerA.Enable();
+    }
+    
+    void OnDisable()
+    {
+        inputController.PlayerA.Disable();
+    }
+}
