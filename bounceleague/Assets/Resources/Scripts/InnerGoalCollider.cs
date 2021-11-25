2455f9098ddafd1df9d1718845eb88f718396a86
Willy Kantana <willy@alegrium.com>
2021-11-25 13:04:17 +0700

    add goal

---
+++ bounceleague/Assets/Resources/Scripts/InnerGoalCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerGoalCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Ball")
+        {
+            TestBallScript ballScript = other.gameObject.GetComponent<TestBallScript>();
+            ballScript.OnGoal(transform.position);
+        }
+    }
+}
