2a6761d812ec697c8cb9dbc6e904d68effc06cab
Yoga <yoga@alegrium.com>
2021-11-25 11:32:46 +0700

    player rotate, add keyboard controller

--- bounceleague/Assets/Resources/Scripts/Character.cs
+++ bounceleague/Assets/Resources/Scripts/Character.cs
@@ -8,7 +8,10 @@ public class Character : MonoBehaviour
 {
     public Rigidbody rigidBody;
     public float speed;
+    public float rotateSpeed;
     Vector2 move;
+    Vector2 rotation;
+    Vector2 lastRotation;
     InputController inputController;
 
     void Awake()
@@ -18,13 +21,23 @@ public class Character : MonoBehaviour
         inputController.PlayerA.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
         inputController.PlayerA.Move.canceled += ctx => move = Vector2.zero;
         
-        inputController.PlayerA.Look.performed += ctx => move = ctx.ReadValue<Vector2>();
-        inputController.PlayerA.Look.canceled += ctx => move = Vector2.zero;
+        inputController.PlayerA.Look.performed += ctx => rotation = ctx.ReadValue<Vector2>();
+        inputController.PlayerA.Look.canceled += ctx => rotation = lastRotation;
     }
 
     void FixedUpdate()
     {
+        //move - left analog
         Vector3 m = new Vector3(move.x,move.y,0);
+        m = m.normalized * speed * Time.deltaTime;
+        rigidBody.MovePosition(transform.position + m);
+
+        //rotate - right analog
+        lastRotation = rotation;
+        float aimAngle = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        Quaternion aimRotation = Quaternion.AngleAxis(aimAngle, Vector3.forward);
+        Quaternion rotate = Quaternion.Slerp(rigidBody.transform.rotation, aimRotation, rotateSpeed * Time.time);
+        rigidBody.MoveRotation(rotate);
     }
 
     void OnEnable()
