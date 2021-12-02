e075417defda5c3393f983d045ae40a55fb5e326
Yoga <yoga@alegrium.com>
2021-12-02 10:14:17 +0700

    - Add projectile behaviour

--- bounceleague/Assets/Resources/Scripts/Character.cs
+++ bounceleague/Assets/Resources/Scripts/Character.cs
@@ -6,13 +6,18 @@ using UnityEngine.InputSystem;
 
 public class Character : MonoBehaviour
 {
+    public Projectile projectile;
     public Rigidbody rigidBody;
+    public Transform launcherPos;
     public float speed;
     public float rotateSpeed;
+    public float shootPerSecond;
+    InputController inputController;
+    Quaternion aimRotation;
     Vector2 move;
     Vector2 rotation;
     Vector2 lastRotation;
-    InputController inputController;
+    float shootTimer;
 
     void Awake()
     {
@@ -25,6 +30,14 @@ public class Character : MonoBehaviour
         inputController.PlayerA.Look.canceled += ctx => rotation = lastRotation;
     }
 
+    void Update()
+    {
+        if(shootTimer > 0)
+        {
+            shootTimer -= Time.deltaTime;
+        }
+    }
+    
     void FixedUpdate()
     {
         //move - left analog
@@ -35,11 +48,34 @@ public class Character : MonoBehaviour
         //rotate - right analog
         lastRotation = rotation;
         float aimAngle = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        Quaternion aimRotation = Quaternion.AngleAxis(aimAngle, Vector3.forward);
+        aimRotation = Quaternion.AngleAxis(aimAngle, Vector3.forward);
         Quaternion rotate = Quaternion.Slerp(rigidBody.transform.rotation, aimRotation, rotateSpeed * Time.time);
         rigidBody.MoveRotation(rotate);
+        
+        //shoot
+        if(rotation.x > 0.5f || rotation.x < -0.5f)
+        {
+            Shoot();
+        }
+        else if(rotation.y > 0.5f || rotation.y < -0.5f)
+        {
+            Shoot();
+        }
     }
 
+    void Shoot()
+    {
+        if(shootTimer <= 0)
+        {
+            Vector3 projectileSpawnPos = launcherPos.position;
+            
+            Projectile p = Instantiate(projectile.gameObject, projectileSpawnPos, aimRotation).GetComponent<Projectile>();
+            p.Init(rotation);
+
+            shootTimer = shootPerSecond;
+        }
+    }
+    
     void OnEnable()
     {
         inputController.PlayerA.Enable();
---
+++ bounceleague/Assets/Resources/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float destroyInSec;
+    public float speed;
+    public Rigidbody rigidbody;
+    float timerDestroy;
+    Vector2 rot;
+
+    public void Init(Vector2 rotation)
+    {
+        timerDestroy = destroyInSec;
+        rot = rotation;
+    }
+
+    void Update()
+    {
+        timerDestroy -= 1 * Time.deltaTime;
+        if(timerDestroy <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3 m = new Vector3(rot.x,rot.y,0);
+        m = m.normalized * speed * Time.deltaTime;
+        rigidbody.MovePosition(transform.position + m);
+    }
+}
