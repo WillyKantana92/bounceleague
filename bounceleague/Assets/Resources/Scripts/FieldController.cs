5ead2fe58143c78e98328c299cc88719224824e9
Yoga <yoga@alegrium.com>
2022-01-27 10:33:51 +0700

    goal only collide with character.

---
+++ bounceleague/Assets/Resources/Scripts/FieldController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldController : MonoBehaviour
+{
+    public Collider[] goalColliders;
+    public Collider ballCollider;
+
+    void Start()
+    {
+        int goalLength = goalColliders.Length;
+        for(int i = 0; i < goalLength; i++)
+        {
+            Physics.IgnoreCollision(goalColliders[i],ballCollider,true);
+        }
+    }
+}
