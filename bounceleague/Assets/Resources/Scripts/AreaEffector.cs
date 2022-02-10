01faedcc57610357addaae29ece4c2e547a5393d
Willy Kantana <willy@alegrium.com>
2022-02-10 11:57:40 +0700

    add area effector

---
+++ bounceleague/Assets/Resources/Scripts/AreaEffector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AreaEffector : MonoBehaviour
+{
+    public Collider collider;
+    public float force;
+    
+    void OnCollisionStay(Collision other)
+    {
+        Debug.Log("Collision : " + other.gameObject.tag);
+        
+        if(other.gameObject.CompareTag("character"))
+        {
+            Character player = other.gameObject.GetComponent<Character>();
+            player.rigidBody.AddRelativeForce(Vector3.right * force);
+        }
+    }
+}
